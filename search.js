71d22a9504cb03365e18afabb11a3ce539ea58be
devorahmos <devorahlmoskovitz@gmail.com>
2023-07-17 13:49:23 +0300

    search nav

--- search.js
+++ search.js
@@ -1,5 +1,15 @@
-//search: any word in string? 
-//Date: dd/mm/yy
-//location (city selection)
-//category: tags (dropdown checkbox, whatever is clicked should appear)
+//checkbox
 
+const selectAllCheckbox = document.getElementById("selectAll");
+
+function toggleCheckboxes() {
+  const checkboxes = document.querySelectorAll(
+    '.dropdown-menu input[type="checkbox"]'
+  );
+
+  for (var i = 0; i < checkboxes.length; i++) {
+    checkboxes[i].checked = selectAllCheckbox.checked;
+  }
+}
+
+selectAllCheckbox.addEventListener("click", toggleCheckboxes);
